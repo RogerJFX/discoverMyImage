2e0b9b76393ed55dcc747e65fc3727f210b79a8c
Roger <roger@crazything.de>
2020-01-23 20:11:50 +0100

    Example images added

---
+++ js/menuHandler.js
@@ -0,0 +1,10 @@
+window.$disc = window.$disc || {};
+(function MenuHandler(self) {
+
+    const menuItems = [];
+
+    function MenuItem() {
+
+    }
+
+})(window.$disc.menuHandler = window.$disc.menuHandler || {});
--- uglify.sh
+++ uglify.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-uglifyjs --compress --mangle --output game.min.js -- js/constants.js js/imageHandler.js js/tileManager.js js/animator.js js/xhrHandler.js
+uglifyjs --compress --mangle --output game.min.js -- \
+    js/constants.js \
+    js/imageHandler.js \
+    js/tileManager.js \
+    js/animator.js \
+    js/xhrHandler.js \
+    js/menuHandler.js
 
 # python correctMin.py
